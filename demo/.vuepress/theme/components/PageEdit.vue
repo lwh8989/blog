2919553bb1748c9028f3d0e1e84707ab65e7e5c1
Lee WonHo <lwh8989@naver.com>
2020-02-16 21:39:37 +0900

    footer 제거

--- demo/.vuepress/theme/components/PageEdit.vue
+++ demo/.vuepress/theme/components/PageEdit.vue
@@ -1,10 +1,5 @@
 <template>
   <footer class="page-edit">
-    <div class="edit-link" v-if="editLink">
-      <a :href="editLink" target="_blank" rel="noopener noreferrer">{{ editLinkText }}</a>
-      <OutboundLink />
-    </div>
-
     <div class="last-updated" v-if="lastUpdated">
       <span class="prefix">{{ lastUpdatedText }}:</span>
       <span class="time">{{ lastUpdated }}</span>
